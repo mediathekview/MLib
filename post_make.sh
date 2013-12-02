c26a812a7c2e1cfb1e2768e17ab3384daf3fe2e3
W.Xaver <w.xaver@googlemail.com>
2013-12-02 20:21:11 +0100

    build.xml 	neue Datei:   build.xml 	geändert:   dist/MSearch.jar 	gelöscht:    dist/info/1.246.build 	neue Datei:   dist/info/1.307.build 	neue Datei:   nbproject/build-impl.xml 	neue Datei:   nbproject/genfiles.properties 	neue Datei:   nbproject/project.properties 	neue Datei:   nbproject/project.xml 	neue Datei:   post_make.sh 	geändert:   src/version.properties

---
+++ post_make.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+if [ $(hostname) = "beta" ]
+then
+# nur für den Entwicklungsrechner sinnvoll
+
+dir=`dirname "$0"`
+cd "$dir"
+
+
+cp -r res/* dist
+cp -r dist/lib/* libs
+
+# Aufräumen
+rm dist/README.TXT
+
+# Anlegen
+mkdir dist/info
+
+# release
+relNr=$(cat src/version.properties | grep BUILD | sed 's#BUILD=##g')
+datum=$(date +%d.%m.%Y )
+echo Datum: $datum >> dist/info/$relNr.build
+echo MSearch Buildnummer: $relNr >> dist/info/$relNr.build
+
+# zip erstellen
+cd dist/
+datum=$(date +%Y.%m.%d )
+zip -r MSearch_$datum.zip .
+
+# Dateien ins share-Verzeichnis von VmWare kopieren
+cp -r dist/* /mnt/daten/virtualbox/share/MSearch
+
+cd $OLDPWD
+
+fi
