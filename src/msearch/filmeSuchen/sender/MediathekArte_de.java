d3340f464c42d22ca1c657a46e16280e8899b811
W.Xaver <w.xaver@googlemail.com>
2015-11-16 12:46:35 +0100

    ARTE

--- src/msearch/filmeSuchen/sender/MediathekArte_de.java
+++ src/msearch/filmeSuchen/sender/MediathekArte_de.java
@@ -119,7 +119,7 @@ public class MediathekArte_de extends MediathekReader implements Runnable {
                 meldungAddThread();
                 addConcert(start, anz);
             } catch (Exception ex) {
-                MSLog.fehlerMeldung(787452309,   ex, "");
+                MSLog.fehlerMeldung(787452309, ex, "");
             }
             meldungThreadUndFertig();
         }
@@ -162,7 +162,7 @@ public class MediathekArte_de extends MediathekReader implements Runnable {
                             }
                         }
                         if (url.equals("")) {
-                            MSLog.fehlerMeldung(825241452,   "keine URL");
+                            MSLog.fehlerMeldung(825241452, "keine URL");
                         } else {
                             urlWeb = "http://concert.arte.tv" + url;
                             meldung(urlWeb);
@@ -174,7 +174,7 @@ public class MediathekArte_de extends MediathekReader implements Runnable {
                             }
                             url = seite2.extract("arte_vp_url=\"", "\"");
                             if (url.isEmpty()) {
-                                MSLog.fehlerMeldung(784512698,  "keine URL");
+                                MSLog.fehlerMeldung(784512698, "keine URL");
                             } else {
                                 seite2 = getUrlIo.getUri_Utf(sendername, url, seite2, "");
                                 int p1 = 0;
@@ -222,10 +222,10 @@ public class MediathekArte_de extends MediathekReader implements Runnable {
                                 if (urlNormal.isEmpty()) {
                                     urlNormal = urlLow;
                                     urlLow = "";
-                                    MSLog.fehlerMeldung(951236487,   "keine URL");
+                                    MSLog.fehlerMeldung(951236487, "keine URL");
                                 }
                                 if (urlNormal.isEmpty()) {
-                                    MSLog.fehlerMeldung(989562301,   "keine URL");
+                                    MSLog.fehlerMeldung(989562301, "keine URL");
                                 } else {
                                     DatenFilm film = new DatenFilm(sendername, THEMA, urlWeb, titel, urlNormal, "" /*urlRtmp*/,
                                             datum, "" /*zeit*/, duration, beschreibung);
@@ -240,7 +240,7 @@ public class MediathekArte_de extends MediathekReader implements Runnable {
                             }
                         }
                     } catch (Exception ex) {
-                        MSLog.fehlerMeldung(465623121,   ex);
+                        MSLog.fehlerMeldung(465623121, ex);
                     }
                 }
             }
@@ -263,7 +263,7 @@ public class MediathekArte_de extends MediathekReader implements Runnable {
                     addTheman(seite1, seite2, link[0]);
                 }
             } catch (Exception ex) {
-                MSLog.fehlerMeldung(894330854,   ex, "");
+                MSLog.fehlerMeldung(894330854, ex, "");
             }
             meldungThreadUndFertig();
         }
@@ -387,7 +387,7 @@ public class MediathekArte_de extends MediathekReader implements Runnable {
         String bild = "";
         String filmWebsite = "";
         String dauerStr = "";
-        String titel = "", thema = "";
+        String titel = "", thema = "", subTitle = "";
         long dauer = 0;
         final String MUSTER_BILD = "programImage\":\"";
         final String MUSTER_BESCHREIBUNG = "\"VDE\":\"";
@@ -460,6 +460,11 @@ public class MediathekArte_de extends MediathekReader implements Runnable {
         datum = convertDatum(arr[1]);
         zeit = convertZeit(arr[2]);
         titel = arr[3];
+        subTitle = seite.extract("\"VSU\":\"", "\",");
+        subTitle = subTitle.replace("\\\"", "\"");
+        if (!subTitle.isEmpty() && !titel.equals(subTitle)) {
+            titel = titel + " - " + subTitle;
+        }
         thema = arr[4];
         if (!url.isEmpty()) {
             //    public DatenFilm(String ssender, String tthema, String filmWebsite, String ttitel, String uurl, String uurlRtmp,
@@ -489,7 +494,7 @@ public class MediathekArte_de extends MediathekReader implements Runnable {
                     datum, zeit, dauer, beschreibung);
             addFilm(film);
         } else {
-            MSLog.fehlerMeldung(963025874,   "Keine URL: " + arr[0]);
+            MSLog.fehlerMeldung(963025874, "Keine URL: " + arr[0]);
         }
     }
 
