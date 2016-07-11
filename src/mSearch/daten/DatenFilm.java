de8d3cfea0fe5e64c1d067b0c128d49ae200debb
W.Xaver <w.xaver@googlemail.com>
2016-07-11 18:36:41 +0200

    Titel Tabelle Filme

--- src/mSearch/daten/DatenFilm.java
+++ src/mSearch/daten/DatenFilm.java
@@ -86,7 +86,7 @@ public class DatenFilm implements Comparable<DatenFilm> {
     public static final String[] COLUMN_NAMES = {"Nr", "Sender", "Thema", "Titel",
         "", "", "Datum", "Zeit", "Dauer", "Größe [MB]", "HD", "UT",
         "Beschreibung", "Geo", "Url", "Website", "Aboname",
-        "URL Untertitel", "Url RTMP", "Url Auth", "Url Klein", "Url RTMP Klein", "Url HD", "Url RTMP HD", "Url History", "neu",
+        "Url Untertitel", "Url RTMP", "Url Auth", "Url Klein", "Url RTMP Klein", "Url HD", "Url RTMP HD", "Url History", "neu",
         "DatumL", "Ref"};
 
     // neue Felder werden HINTEN angefügt!!!!!
