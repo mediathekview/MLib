508bb233c2f3fef21e553dc57bafb3c1745ae75d
Christian F <crystalpalace1977@googlemail.com>
2017-01-03 09:26:26 +0100

    -add more error logging to update search -move code to MV which is only used there

--- src/main/java/mSearch/tool/Log.java
+++ src/main/java/mSearch/tool/Log.java
@@ -19,13 +19,13 @@
  */
 package mSearch.tool;
 
+import mSearch.Config;
+import mSearch.Const;
+
 import java.io.*;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.LinkedList;
-import mSearch.Config;
-import mSearch.Const;
 
 public class Log {
 
@@ -65,88 +65,12 @@ public class Log {
 
     }
 
-    public static synchronized void versionMsg(String progName) {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
-        sysLog("");
-        sysLog("");
-        sysLog("");
-        sysLog("");
-        sysLog("");
-        sysLog("___  ___         _ _       _   _          _    _   _ _               ");
-        sysLog("|  \\/  |        | (_)     | | | |        | |  | | | (_)              ");
-        sysLog("| .  . | ___  __| |_  __ _| |_| |__   ___| | _| | | |_  _____      __");
-        sysLog("| |\\/| |/ _ \\/ _` | |/ _` | __| '_ \\ / _ \\ |/ / | | | |/ _ \\ \\ /\\ / /");
-        sysLog("| |  | |  __/ (_| | | (_| | |_| | | |  __/   <\\ \\_/ / |  __/\\ V  V / ");
-        sysLog("\\_|  |_/\\___|\\__,_|_|\\__,_|\\__|_| |_|\\___|_|\\_\\\\___/|_|\\___| \\_/\\_/  ");
-        sysLog("");
-        sysLog("");
-        sysLog(LILNE);
-        sysLog("Programmstart: " + sdf.format(startZeit));
-        sysLog(LILNE);
-        sysLog("");
-        long totalMem = Runtime.getRuntime().totalMemory();
-        sysLog("totalMemory: " + totalMem / (1000L * 1000L) + " MB");
-        long maxMem = Runtime.getRuntime().maxMemory();
-        sysLog("maxMemory: " + maxMem / (1000L * 1000L) + " MB");
-        long freeMem = Runtime.getRuntime().freeMemory();
-        sysLog("freeMemory: " + freeMem / (1000L * 1000L) + " MB");
-        sysLog("");
-        sysLog(LILNE);
-        sysLog("");
-        //Version
-        sysLog(progName + Functions.getProgVersionString());
-        String compile = Functions.getCompileDate();
-        if (!compile.isEmpty()) {
-            sysLog("Compiled: " + compile);
-        }
-        sysLog("");
-        sysLog(LILNE);
-        sysLog("");
-        sysLog("Java");
-        String[] java = Functions.getJavaVersion();
-        for (String ja : java) {
-            Log.sysLog(ja);
-        }
-        sysLog("");
-    }
-
-    public static synchronized void endMsg() {
-        sysLog("");
-        sysLog("");
-        sysLog("");
-        sysLog("");
-
-        printErrorMsg().forEach(Log::sysLog);
-
-        // Laufzeit ausgeben
-        Date stopZeit = new Date(System.currentTimeMillis());
-        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
-        int minuten;
-        try {
-            minuten = Math.round((stopZeit.getTime() - startZeit.getTime()) / (1000 * 60));
-        } catch (Exception ex) {
-            minuten = -1;
-        }
-        sysLog("");
-        sysLog("");
-        sysLog(LILNE);
-        sysLog("   --> Beginn: " + sdf.format(startZeit));
-        sysLog("   --> Fertig: " + sdf.format(stopZeit));
-        sysLog("   --> Dauer[Min]: " + (minuten == 0 ? "<1" : minuten));
-        sysLog(LILNE);
-        sysLog("");
-        sysLog("   und Tschuess");
-        sysLog("");
-        sysLog("");
-        sysLog(LILNE);
-    }
-
     public static synchronized ArrayList<String> printErrorMsg() {
         int max = 0;
         ArrayList<String> retList = new ArrayList<>();
         retList.add("");
         retList.add(LILNE);
-        if (fehlerListe.size() == 0) {
+        if (fehlerListe.isEmpty()) {
             retList.add(" Keine Fehler :)");
         } else {
             // Fehler ausgeben
@@ -160,7 +84,7 @@ public class Log {
             max++;
             for (Error e : fehlerListe) {
                 while (e.cl.length() < max) {
-                    e.cl = e.cl + " ";
+                    e.cl = e.cl + ' ';
                 }
             }
             for (int i = 1; i < fehlerListe.size(); ++i) {
@@ -221,7 +145,7 @@ public class Log {
     public static synchronized void progress(String texte) {
         progress = true;
         if (!texte.isEmpty()) {
-            System.out.print(texte + "\r");
+            System.out.print(texte + '\r');
         }
     }
 
@@ -247,7 +171,7 @@ public class Log {
     private static void fehlermeldung_(int fehlerNummer, Exception ex, String[] texte) {
         final Throwable t = new Throwable();
         final StackTraceElement methodCaller = t.getStackTrace()[2];
-        final String klasse = methodCaller.getClassName() + "." + methodCaller.getMethodName();
+        final String klasse = methodCaller.getClassName() + '.' + methodCaller.getMethodName();
         String kl;
         try {
             kl = klasse;
@@ -255,7 +179,7 @@ public class Log {
                 if (Character.isUpperCase(kl.charAt(0))) {
                     break;
                 } else {
-                    kl = kl.substring(kl.indexOf(".") + 1);
+                    kl = kl.substring(kl.indexOf('.') + 1);
                 }
             }
         } catch (Exception ignored) {
@@ -292,7 +216,7 @@ public class Log {
             if (ex != null) {
                 logList.add(z + " Exception: " + ex.getMessage());
             }
-            logList.add(z + " " + FEHLER + kl);
+            logList.add(z + ' ' + FEHLER + kl);
             for (String aTexte : texte) {
                 logList.add(z + "           " + aTexte);
             }
@@ -305,7 +229,7 @@ public class Log {
         resetProgress();
         final String z = ". ";
         if (texte.length <= 1) {
-            logList.add(z + " " + texte[0]);
+            logList.add(z + ' ' + texte[0]);
         } else {
             String zeile = "---------------------------------------";
             String txt;
