408c33f354ec302e17884db72425005af0e16a54
Christian F <crystalpalace1977@googlemail.com>
2017-02-21 21:45:07 +0100

    forgot ti import file

---
+++ src/main/java/mSearch/tool/MVHttpClient.java
@@ -0,0 +1,31 @@
+package mSearch.tool;
+
+import okhttp3.ConnectionPool;
+import okhttp3.OkHttpClient;
+
+import java.util.concurrent.TimeUnit;
+
+public class MVHttpClient {
+    private final static MVHttpClient ourInstance = new MVHttpClient();
+    //private static final Cache cache = new Cache(new File("./cache"), 1024L * 1024L * 200L);
+    private final OkHttpClient httpClient;
+
+    private MVHttpClient() {
+        httpClient = new OkHttpClient.Builder()
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .connectionPool(new ConnectionPool(100, 1, TimeUnit.SECONDS))
+                .build();
+        //.cache(cache).build();
+        httpClient.dispatcher().setMaxRequests(100);
+    }
+
+    public static MVHttpClient getInstance() {
+        return ourInstance;
+    }
+
+    public OkHttpClient getHttpClient() {
+        return httpClient;
+    }
+}
