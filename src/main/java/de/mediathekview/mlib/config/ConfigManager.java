854216c6d4b0984ab06841fedf5b049dc6f0419c
Nicklas Wiegandt <nicklas@wiegandt.net>
2017-07-22 02:23:48 +0200

    Crated base files for a configuration manager

---
+++ src/main/java/de/mediathekview/mlib/config/ConfigManager.java
@@ -0,0 +1,35 @@
+package de.mediathekview.mlib.config;
+
+import org.cfg4j.provider.ConfigurationProvider;
+import org.cfg4j.provider.ConfigurationProviderBuilder;
+import org.cfg4j.source.ConfigurationSource;
+import org.cfg4j.source.context.filesprovider.ConfigFilesProvider;
+import org.cfg4j.source.files.FilesConfigurationSource;
+
+import java.nio.file.Path;
+
+/**
+ * A manager to load configurations.
+ */
+public abstract class ConfigManager<T extends ConfigDTO>
+{
+    private T config;
+
+    abstract Path getConfigFilePath();
+    abstract String getConfigName();
+    abstract Class<T> getConfigClass();
+
+    ConfigManager()
+    {
+        ConfigurationSource configurationSource = new FilesConfigurationSource(() -> getConfigFilePath());
+        final ConfigurationProvider configurationProvider = new ConfigurationProviderBuilder()
+                .withConfigurationSource(configurationSource)
+                .build();
+        config = configurationProvider.bind(getConfigName(),getConfigClass());
+    }
+
+    public T getConfig()
+    {
+        return config;
+    }
+}
