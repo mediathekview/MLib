284e48d2c35051596e050d2e7c0b5ead3f906f94
Nicklas Wiegandt <nicklas@wiegandt.net>
2017-10-02 21:40:21 +0200

    Changed visibility

--- src/main/java/de/mediathekview/mlib/daten/Resolution.java
+++ src/main/java/de/mediathekview/mlib/daten/Resolution.java
@@ -6,90 +6,87 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
-public enum Resolution
-{
-    HD        (3, "HD"), 
-    NORMAL    (2, "Normal"), 
-    SMALL     (1, "Klein"), 
-    VERY_SMALL(0, "Sehr klein")
-    ;
+public enum Resolution {
+  HD(3, "HD"), NORMAL(2, "Normal"), SMALL(1, "Klein"), VERY_SMALL(0, "Sehr klein");
 
-    /**
-     * The bigger the index the better the quality.
-     */
-    private final int resolutionSize;
-    private final String description;
+  private enum CountingDirection {
+    HIGHER(+1), LOWER(-1);
 
-    private Resolution(final int resolutionSize, final String description)
-    {
-        this.resolutionSize = resolutionSize;
-        this.description    = description;
+    int direction;
+
+    private CountingDirection(final int direction) {
+      this.direction = direction;
     }
 
-    public String getDescription()
-    {
-        return description;
+    public int getDirectionValue() {
+      return direction;
+    }
+  }
+
+  /**
+   * The bigger the index the better the quality.
+   */
+  private final int resolutionSize;
+
+  private final String description;
+
+  private Resolution(final int resolutionSize, final String description) {
+    this.resolutionSize = resolutionSize;
+    this.description = description;
+  }
+
+  public static List<Resolution> getFromBestToLowest() {
+    return Arrays.asList(Resolution.values()).stream()
+        .sorted(Comparator.comparing(Resolution::getResolutionSize).reversed())
+        .collect(Collectors.toList());
+  }
+
+  public static Resolution getHighestResolution() {
+    return Resolution.HD;
+  }
+
+  public static Resolution getLowestResolution() {
+    return Resolution.VERY_SMALL;
+  }
+
+  public static Resolution getNextHigherResolution(final Resolution startingResolution) {
+    return getNextResolutionByDirection(startingResolution, CountingDirection.HIGHER);
+  }
+
+  public static Resolution getNextLowerResolution(final Resolution startingResolution) {
+    return getNextResolutionByDirection(startingResolution, CountingDirection.LOWER);
+  }
+
+  static Resolution getNextResolutionByDirection(final Resolution startingResolution,
+      final CountingDirection direction) {
+    try {
+      return getResoultionByResolutionSize(
+          startingResolution.getResolutionSize() + direction.getDirectionValue());
+    } catch (final NoSuchElementException nsee) {
+      return startingResolution;
+    }
+  }
+
+  static Resolution getResoultionByResolutionSize(final int searchedResolutionSize)
+      throws NoSuchElementException {
+    for (final Resolution currentResolution : Resolution.values()) {
+      if (searchedResolutionSize == currentResolution.getResolutionSize()) {
+        return currentResolution;
+      }
     }
 
-    public static Resolution getHighestResolution() {
-        return Resolution.HD;
-    }
-    
-    public static Resolution getLowestResolution() {
-        return Resolution.VERY_SMALL;
-    }
+    throw new NoSuchElementException(
+        String.format("Resolution with ResolutionIndex %d not found", searchedResolutionSize));
+  }
 
-    public static List<Resolution> getFromBestToLowest()
-    {
-        return Arrays.asList(Resolution.values()).stream().sorted(Comparator.comparing(Resolution::getResolutionSize).reversed())
-                .collect(Collectors.toList());
-    }
+  public String getDescription() {
+    return description;
+  }
 
-    public static Resolution getNextHigherResolution(Resolution startingResolution) {
-        return getNextResolutionByDirection(startingResolution, CountingDirection.HIGHER);
-    }
-    
-    public static Resolution getNextLowerResolution(Resolution startingResolution) {
-        return getNextResolutionByDirection(startingResolution, CountingDirection.LOWER);
-    }
-    
-    int getResolutionSize() {
-        return resolutionSize;
-    }
 
-    static Resolution getResoultionByResolutionSize(int searchedResolutionSize) throws NoSuchElementException {
-        for (Resolution currentResolution : Resolution.values()) {
-            if(searchedResolutionSize == currentResolution.getResolutionSize()) {
-                return currentResolution;
-            }
-        }
-        
-        throw new NoSuchElementException(String.format("Resolution with ResolutionIndex %d not found", searchedResolutionSize));
-    }
+  public int getResolutionSize() {
+    return resolutionSize;
+  }
 
-    private enum CountingDirection {
-        HIGHER(+1),
-        LOWER(-1);
-        
-        int direction;
-        
-        private CountingDirection(int direction) {
-            this.direction = direction;
-        }
-        
-        public int getDirectionValue() {
-            return direction;
-        }
-    }
-    
-    
-    static Resolution getNextResolutionByDirection(Resolution startingResolution, CountingDirection direction) {
-        try {
-            return getResoultionByResolutionSize((startingResolution.getResolutionSize() + direction.getDirectionValue()));
-        } catch (NoSuchElementException nsee) {
-            return startingResolution;
-        }
-    }
-    
-    
+
 }
