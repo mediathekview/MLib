c421000bf865d3a94265623bb99a099265456b80
Sascha <TheSasch_github@wiegandt.net>
2017-10-09 22:38:17 +0200

    New Functionality
    WebAccessHelper and WebTargetDS to make a faster pool based access to
    the WebTargets

---
+++ src/main/java/de/mediathekview/mlib/datasources/WebTargetDS.java
@@ -0,0 +1,38 @@
+/*
+ * WebTargetDS.java
+ * 
+ * Projekt    : MServer
+ * erstellt am: 05.10.2017
+ * Autor      : Sascha
+ * 
+ * (c) 2017 by Sascha Wiegandt
+ */
+package de.mediathekview.mlib.datasources;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+public class WebTargetDS {
+    
+    private static final Map<String, WebTarget> connectionPool = new ConcurrentHashMap<>();
+    
+    private WebTargetDS() {
+        
+    }
+    
+    public static WebTarget getInstance(String url) {
+        if (connectionPool.containsKey(url)) {
+            return connectionPool.get(url);
+        } else {
+            Client client = ClientBuilder.newClient();
+            connectionPool.put(url, client.target(url));
+
+            return connectionPool.get(url);
+        }
+    }
+
+}
