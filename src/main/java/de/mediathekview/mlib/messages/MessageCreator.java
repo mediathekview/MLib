fe10657eff5aa1eda05ef022a58cdfd8e5b6e748
Nicklas Wiegandt <nicklas@wiegandt.net>
2017-12-22 12:55:31 +0100

    Fixed errors in logging

--- src/main/java/de/mediathekview/mlib/messages/MessageCreator.java
+++ src/main/java/de/mediathekview/mlib/messages/MessageCreator.java
@@ -3,48 +3,39 @@ package de.mediathekview.mlib.messages;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.concurrent.ConcurrentHashMap;
-
 import de.mediathekview.mlib.messages.listener.MessageListener;
 
-public abstract class MessageCreator
-{
-    private final Collection<MessageListener> messageListeners;
+public abstract class MessageCreator {
+  protected final Collection<MessageListener> messageListeners;
 
-    public MessageCreator()
-    {
-        super();
-        messageListeners = ConcurrentHashMap.newKeySet();
-    }
+  public MessageCreator() {
+    super();
+    messageListeners = ConcurrentHashMap.newKeySet();
+  }
 
-    public MessageCreator(final MessageListener... aListeners)
-    {
-        this();
-        messageListeners.addAll(Arrays.asList(aListeners));
-    }
+  public MessageCreator(final MessageListener... aListeners) {
+    this();
+    messageListeners.addAll(Arrays.asList(aListeners));
+  }
 
-    public boolean addMessageListener(final MessageListener aMessageListener)
-    {
-        return messageListeners.add(aMessageListener);
-    }
+  public boolean addAllMessageListener(final Collection<MessageListener> aMessageListeners) {
+    return messageListeners.addAll(aMessageListeners);
+  }
 
-    public boolean removeMessageListener(final MessageListener aMessageListener)
-    {
-        return messageListeners.remove(aMessageListener);
-    }
+  public boolean addMessageListener(final MessageListener aMessageListener) {
+    return messageListeners.add(aMessageListener);
+  }
 
-    public boolean addAllMessageListener(final Collection<MessageListener> aMessageListeners)
-    {
-        return messageListeners.addAll(aMessageListeners);
-    }
+  public boolean removeAllMessageListener(final Collection<MessageListener> aMessageListeners) {
+    return messageListeners.removeAll(aMessageListeners);
+  }
 
-    public boolean removeAllMessageListener(final Collection<MessageListener> aMessageListeners)
-    {
-        return messageListeners.removeAll(aMessageListeners);
-    }
+  public boolean removeMessageListener(final MessageListener aMessageListener) {
+    return messageListeners.remove(aMessageListener);
+  }
 
-    protected void publishMessage(final Message aMessage, final Object... aParams)
-    {
-        messageListeners.parallelStream().forEach(l -> l.consumeMessage(aMessage, aParams));
-    }
+  protected void publishMessage(final Message aMessage, final Object... aParams) {
+    messageListeners.parallelStream().forEach(l -> l.consumeMessage(aMessage, aParams));
+  }
 
 }
