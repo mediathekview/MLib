a31460f03ff65875af631397d2fdfff37465877f
Nicklas Wiegandt <nicklas@wiegandt.net>
2018-01-27 19:50:25 +0100

    Changes vor MServer/#311

--- src/main/java/de/mediathekview/mlib/daten/GeoLocations.java
+++ src/main/java/de/mediathekview/mlib/daten/GeoLocations.java
@@ -7,9 +7,14 @@ import org.apache.commons.lang3.StringUtils;
  * The available GEO locations.
  */
 public enum GeoLocations {
-  GEO_NONE("", "WELT", "none", "world", ""), // nur in .. zu sehen
-  GEO_DE("DE"), GEO_AT("AT"), GEO_CH("CH"), GEO_EU("EU"), GEO_DE_FR("DE-FR"), GEO_DE_AT_CH(
-      "DE-AT-CH", "dach"), GEO_DE_AT_CH_EU("DE-AT-CH-EU");
+  GEO_NONE("", "WELT", "none", "world", "ALL", ""), // nur in .. zu sehen
+  GEO_DE("DE"),
+  GEO_AT("AT"),
+  GEO_CH("CH"),
+  GEO_EU("EU", "EUR_DE_FR"),
+  GEO_DE_FR("DE-FR", "DE_FR"),
+  GEO_DE_AT_CH("DE-AT-CH", "dach"),
+  GEO_DE_AT_CH_EU("DE-AT-CH-EU", "SAT");
 
   private final String description;
   private String[] alternatives;
