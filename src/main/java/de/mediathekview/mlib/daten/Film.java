136e97c5bb72b25711ed634cc7d00dfa770fd82c
pidoubleyou <pidoubleyou@arcor.de>
2018-02-20 19:20:32 +0100

    Film: Umstellung von Listen auf Map für Gebärdensprachen- und Audiodeskriptions-URLs

--- src/main/java/de/mediathekview/mlib/daten/Film.java
+++ src/main/java/de/mediathekview/mlib/daten/Film.java
@@ -5,6 +5,8 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.EnumMap;
+import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 
@@ -13,36 +15,36 @@ import java.util.UUID;
  */
 public class Film extends Podcast {
   private static final long serialVersionUID = -7834270191129532291L;
-  private final Collection<URL> audioDescriptions;
-  private final Collection<URL> signLanguages;
+  private final Map<Resolution, FilmUrl> audioDescriptions;
+  private final Map<Resolution, FilmUrl> signLanguages;
   private final Collection<URL> subtitles;
 
   public Film(final UUID aUuid, final Sender aSender, final String aTitel, final String aThema,
       final LocalDateTime aTime, final Duration aDauer) {
     super(aUuid, aSender, aTitel, aThema, aTime, aDauer);
-    audioDescriptions = new ArrayList<>();
-    signLanguages = new ArrayList<>();
+    audioDescriptions = new EnumMap<>(Resolution.class);
+    signLanguages = new EnumMap<>(Resolution.class);
     subtitles = new ArrayList<>();
   }
 
-  public void addAudioDescription(final URL aAudioDescriptionUrl) {
-    if (aAudioDescriptionUrl != null) {
-      audioDescriptions.add(aAudioDescriptionUrl);
+  public void addAudioDescription(final Resolution aQuality, final FilmUrl aUrl) {
+    if (aQuality != null && aUrl != null) {
+      audioDescriptions.put(aQuality, aUrl);
     }
   }
 
-  public Collection<URL> getAudioDescriptions() {
-    return new ArrayList<>(audioDescriptions);
+  public FilmUrl getAudioDescription(final Resolution aQuality) {
+    return audioDescriptions.get(aQuality);
   }
 
-  public void addSignLanguage(final URL aSignLanguageUrl) {
-    if (aSignLanguageUrl != null) {
-      signLanguages.add(aSignLanguageUrl);
+  public void addSignLanguage(final Resolution aQuality, final FilmUrl aUrl) {
+    if (aQuality != null && aUrl != null) {
+      signLanguages.put(aQuality, aUrl);
     }
   }
 
-  public Collection<URL> getSignLanguages() {
-    return new ArrayList<>(signLanguages);
+  public FilmUrl getSignLanguage(final Resolution aQuality) {
+    return signLanguages.get(aQuality);
   }
   
   public void addSubtitle(final URL aSubtitleUrl) {
