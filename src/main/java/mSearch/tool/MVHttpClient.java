9de76e006c77e5bf5838c77f87f82bd8aac5fe07
Christian F <crystalpalace1977@googlemail.com>
2018-04-24 21:56:18 +0200

    - respect proxy environment variables with okhttp - remove unneeded connection pool

--- src/main/java/mSearch/tool/MVHttpClient.java
+++ src/main/java/mSearch/tool/MVHttpClient.java
@@ -1,28 +1,82 @@
 package mSearch.tool;
 
-import okhttp3.ConnectionPool;
 import okhttp3.OkHttpClient;
 
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 import java.util.concurrent.TimeUnit;
 
 public class MVHttpClient {
     private final static MVHttpClient ourInstance = new MVHttpClient();
-    private final OkHttpClient httpClient;
-    private final OkHttpClient copyClient;
+    private OkHttpClient httpClient;
+    private OkHttpClient copyClient;
 
     private MVHttpClient() {
-        httpClient = new OkHttpClient.Builder()
+        final String proxyHost = System.getProperty("http.proxyHost");
+        final String proxyPort = System.getProperty("http.proxyPort");
+
+        if (proxyHost != null && proxyPort != null) {
+            //setup for proxy
+            try {
+                final Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, Integer.parseInt(proxyPort)));
+                setupProxyClients(proxy);
+            } catch (NumberFormatException ex) {
+                Log.errorLog(123456789, ex, "PROXY config failed. Creating non proxy config");
+                //in case of error with proxy configuration log error and create no proxy config
+                setupNonProxyClients();
+            }
+        } else {
+            //TODO setup proxy from settings file
+
+            //no proxy setup
+            setupNonProxyClients();
+        }
+
+    }
+
+    /**
+     * Build the client builder with default settings.
+     *
+     * @return A Builder with default settings.
+     */
+    private OkHttpClient.Builder getDefaultClientBuilder() {
+        return new OkHttpClient.Builder()
                 .connectTimeout(30, TimeUnit.SECONDS)
                 .writeTimeout(30, TimeUnit.SECONDS)
-                .readTimeout(30, TimeUnit.SECONDS)
-                .connectionPool(new ConnectionPool(100, 1, TimeUnit.SECONDS))
+                .readTimeout(30, TimeUnit.SECONDS);
+    }
+
+    /**
+     * Set the proxy parameters on the shared HTTP clients.
+     *
+     * @param proxy The proxy settings to be used.
+     */
+    private void setupProxyClients(Proxy proxy) {
+        httpClient = getDefaultClientBuilder()
+                .proxy(proxy)
                 .build();
-        httpClient.dispatcher().setMaxRequests(100);
 
         copyClient = httpClient.newBuilder()
                 .connectTimeout(5, TimeUnit.SECONDS)
                 .readTimeout(5, TimeUnit.SECONDS)
-                .writeTimeout(2, TimeUnit.SECONDS).build();
+                .writeTimeout(2, TimeUnit.SECONDS)
+                .proxy(proxy)
+                .build();
+
+    }
+
+    /**
+     * Setup HTTP client without proxy settings
+     */
+    private void setupNonProxyClients() {
+        httpClient = getDefaultClientBuilder()
+                .build();
+
+        copyClient = httpClient.newBuilder()
+                .connectTimeout(5, TimeUnit.SECONDS)
+                .readTimeout(5, TimeUnit.SECONDS)
+                .writeTimeout(2, TimeUnit.SECONDS)
+                .build();
     }
 
     public static MVHttpClient getInstance() {
