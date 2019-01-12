1392f865f623548f69bf1d9a8638e5a32ed7eb4b
Nicklas Wiegandt <nicklas@pop-os.localdomain>
2019-01-12 15:31:27 +0100

    Added details for maven central deployment

--- src/main/java/de/mediathekview/mlib/tool/SysMsg.java
+++ src/main/java/de/mediathekview/mlib/tool/SysMsg.java
@@ -15,14 +15,8 @@
  */
 package de.mediathekview.mlib.tool;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-
 @Deprecated
 public class SysMsg {
-
-  public static ObservableList<String> textSystem = FXCollections.observableArrayList();
-  public static ObservableList<String> textProgramm = FXCollections.observableArrayList();
 
   public static boolean playerMeldungenAus = false;
   public static final int LOG_SYSTEM = 1;
@@ -34,106 +28,32 @@ public class SysMsg {
   private static int zeilenNrProgramm = 0;
 
   public static void clearText(final int art) {
-    switch (art) {
-      case LOG_SYSTEM:
-        zeilenNrSystem = 0;
-        textSystem.clear();
-        break;
-      case LOG_PLAYER:
-        zeilenNrProgramm = 0;
-        textProgramm.clear();
-        break;
-      default:
-        break;
-    }
   }
 
   public synchronized static String getText(final int logArt) {
-    // wegen synchronized hier
-    if (logArt == SysMsg.LOG_SYSTEM) {
-      return String.join("", textSystem);
-    } else {
-      return String.join("", textProgramm);
-    }
+    return null;
   }
 
   public static synchronized void playerMsg(final String text) {
-    if (!playerMeldungenAus) {
-      playermeldung(new String[] {text});
-    }
   }
 
   public static synchronized void sysMsg(final String text) {
-    systemmeldung(new String[] {text});
   }
 
   public static synchronized void sysMsg(final String[] text) {
-    systemmeldung(text);
-  }
-
-  private synchronized static void addText(final ObservableList<String> text, final String texte) {
-    if (text.size() > MAX_LAENGE_1) {
-      text.remove(0, MAX_LAENGE_2);
-    }
-    text.add(texte + System.getProperty("line.separator"));
   }
 
   private static String getNr(final int nr) {
-    final int MAX_STELLEN = 5;
-    final String FUELL_ZEICHEN = "0";
-    String str = String.valueOf(nr);
-    while (str.length() < MAX_STELLEN) {
-      str = FUELL_ZEICHEN + str;
-    }
-    return str;
+    return null;
   }
 
   private static void notify(final int art, final String zeile) {
-    switch (art) {
-      case LOG_SYSTEM:
-        addText(textSystem, "[" + getNr(zeilenNrSystem++) + "]   " + zeile);
-        break;
-      case LOG_PLAYER:
-        addText(textProgramm, "[" + getNr(zeilenNrProgramm++) + "]   " + zeile);
-        break;
-      default:
-        break;
-    }
-    // Listener.notify(art, SysMsg.class.getSimpleName()); gibt sonst einen deadlock
   }
 
   private static void playermeldung(final String[] texte) {
-    final String z = "  >>";
-    System.out.println(z + " " + texte[0]);
-    notify(LOG_PLAYER, texte[0]);
-    for (int i = 1; i < texte.length; ++i) {
-      System.out.println(z + " " + texte[i]);
-      notify(LOG_PLAYER, texte[i]);
-    }
   }
 
   private static void systemmeldung(final String[] texte) {
-    final String z = ". ";
-    if (texte.length <= 1) {
-      System.out.println(z + " " + texte[0]);
-      notify(LOG_SYSTEM, texte[0]);
-    } else {
-      final String zeile = "----------------------------------------                    ";
-      String txt;
-      System.out.println(z + zeile);
-      notify(LOG_SYSTEM, zeile);
-      for (int i = 0; i < texte.length; ++i) {
-        txt = "| " + texte[i];
-        System.out.println(z + txt);
-        if (i == 0) {
-          notify(LOG_SYSTEM, texte[i]);
-        } else {
-          notify(LOG_SYSTEM, "    " + texte[i]);
-        }
-      }
-      notify(LOG_SYSTEM, " ");
-      System.out.println(z + zeile);
-    }
   }
 
 }
