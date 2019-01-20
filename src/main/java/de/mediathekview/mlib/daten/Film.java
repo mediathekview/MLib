f1ae6b281736768e2c55eb9b075e87cdf3c799d8
Nicklas Wiegandt Nicklas2751 <nicklas@wiegandt.eu>
2019-01-20 18:02:14 +0100

    Pom cleanup and new Film getters

--- src/main/java/de/mediathekview/mlib/daten/Film.java
+++ src/main/java/de/mediathekview/mlib/daten/Film.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.EnumMap;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
@@ -14,60 +15,67 @@ import java.util.UUID;
  * Represents a found film.
  */
 public class Film extends Podcast {
-  private static final long serialVersionUID = -7834270191129532291L;
-  private final Map<Resolution, FilmUrl> audioDescriptions;
-  private final Map<Resolution, FilmUrl> signLanguages;
-  private final Collection<URL> subtitles;
+	private static final long serialVersionUID = -7834270191129532291L;
 
-  public Film(final UUID aUuid, final Sender aSender, final String aTitel, final String aThema,
-      final LocalDateTime aTime, final Duration aDauer) {
-    super(aUuid, aSender, aTitel, aThema, aTime, aDauer);
-    audioDescriptions = new EnumMap<>(Resolution.class);
-    signLanguages = new EnumMap<>(Resolution.class);
-    subtitles = new ArrayList<>();
-  }
+	private final Map<Resolution, FilmUrl> audioDescriptions;
 
-  public void addAudioDescription(final Resolution aQuality, final FilmUrl aUrl) {
-    if (aQuality != null && aUrl != null) {
-      audioDescriptions.put(aQuality, aUrl);
-    }
-  }
+	private final Map<Resolution, FilmUrl> signLanguages;
+	private final Collection<URL> subtitles;
+	public Film(final UUID aUuid, final Sender aSender, final String aTitel, final String aThema,
+			final LocalDateTime aTime, final Duration aDauer) {
+		super(aUuid, aSender, aTitel, aThema, aTime, aDauer);
+		audioDescriptions = new EnumMap<>(Resolution.class);
+		signLanguages = new EnumMap<>(Resolution.class);
+		subtitles = new ArrayList<>();
+	}
+	public void addAllSubtitleUrls(Set<URL> urlsToAdd) {
+		this.subtitles.addAll(urlsToAdd);
+	}
 
-  public FilmUrl getAudioDescription(final Resolution aQuality) {
-    return audioDescriptions.get(aQuality);
-  }
+	public void addAudioDescription(final Resolution aQuality, final FilmUrl aUrl) {
+		if (aQuality != null && aUrl != null) {
+			audioDescriptions.put(aQuality, aUrl);
+		}
+	}
 
-  public void addSignLanguage(final Resolution aQuality, final FilmUrl aUrl) {
-    if (aQuality != null && aUrl != null) {
-      signLanguages.put(aQuality, aUrl);
-    }
-  }
+	public void addSignLanguage(final Resolution aQuality, final FilmUrl aUrl) {
+		if (aQuality != null && aUrl != null) {
+			signLanguages.put(aQuality, aUrl);
+		}
+	}
 
-  public FilmUrl getSignLanguage(final Resolution aQuality) {
-    return signLanguages.get(aQuality);
-  }
-  
-  public void addSubtitle(final URL aSubtitleUrl) {
-    if (aSubtitleUrl != null) {
-      subtitles.add(aSubtitleUrl);
-    }
-  }
+	public void addSubtitle(final URL aSubtitleUrl) {
+		if (aSubtitleUrl != null) {
+			subtitles.add(aSubtitleUrl);
+		}
+	}
 
-  public Collection<URL> getSubtitles() {
-    return new ArrayList<>(subtitles);
-  }
+	public FilmUrl getAudioDescription(final Resolution aQuality) {
+		return audioDescriptions.get(aQuality);
+	}
 
+	public Map<Resolution, FilmUrl> getAudioDescriptions() {
+		return new HashMap<>(audioDescriptions);
+	}
 
-  public boolean hasUT() {
-    return !subtitles.isEmpty();
-  }
+	public FilmUrl getSignLanguage(final Resolution aQuality) {
+		return signLanguages.get(aQuality);
+	}
 
-  public void addAllSubtitleUrls(Set<URL> urlsToAdd) {
-    this.subtitles.addAll(urlsToAdd);
-  }
-  
-  @Override
-  public String toString() {
-    return "Film [subtitles=" + subtitles + "]";
-  }
+	public Map<Resolution, FilmUrl> getSignLanguages() {
+		return new HashMap<>(signLanguages);
+	}
+
+	public Collection<URL> getSubtitles() {
+		return new ArrayList<>(subtitles);
+	}
+
+	public boolean hasUT() {
+		return !subtitles.isEmpty();
+	}
+
+	@Override
+	public String toString() {
+		return "Film [subtitles=" + subtitles + "]";
+	}
 }
