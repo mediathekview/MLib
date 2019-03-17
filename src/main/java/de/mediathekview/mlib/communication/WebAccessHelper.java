5b9ccffd838a522351238e7b53844dbf83bdfcb1
Nicklas Wiegandt <nicklas@wiegandt.eu>
2019-03-17 13:23:05 +0100

    Working on #393 Added timeout to all JSoup calls and the REST task.

--- src/main/java/de/mediathekview/mlib/communication/WebAccessHelper.java
+++ src/main/java/de/mediathekview/mlib/communication/WebAccessHelper.java
@@ -1,36 +1,35 @@
 /*
  * WebAccessHelper.java
- * 
+ *
  * Projekt    : MLib
  * erstellt am: 04.10.2017
  * Autor      : Sascha
- * 
+ *
  */
 package de.mediathekview.mlib.communication;
 
-import java.net.URL;
+import de.mediathekview.mlib.datasources.WebTargetDS;
 
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
-
-import de.mediathekview.mlib.datasources.WebTargetDS;
+import java.net.URL;
 
 public class WebAccessHelper {
-    
-    private WebAccessHelper() {
-        
-    }
-    
-    public static String getJsonResultFromPostAccess(URL serverUrl, String request) throws IllegalArgumentException {
-        if(null == serverUrl)
-            throw new IllegalArgumentException("Es wurde keine gültige ServerURL angegeben");
-        
-        WebTarget target = WebTargetDS.getInstance(serverUrl.toString());
-        
-        return target.request(MediaType.APPLICATION_JSON_TYPE)
-                              .post(Entity.entity(request, MediaType.APPLICATION_JSON_TYPE), String.class);
 
+  private WebAccessHelper() {}
+
+  public static String getJsonResultFromPostAccess(
+      final URL serverUrl, final String request, final long timeoutInSeconds)
+      throws IllegalArgumentException {
+    if (null == serverUrl) {
+      throw new IllegalArgumentException("Es wurde keine gültige ServerURL angegeben");
     }
 
+    final WebTarget target = WebTargetDS.getInstance(serverUrl.toString(), timeoutInSeconds);
+
+    return target
+        .request(MediaType.APPLICATION_JSON_TYPE)
+        .post(Entity.entity(request, MediaType.APPLICATION_JSON_TYPE), String.class);
+  }
 }
--- src/main/java/de/mediathekview/mlib/datasources/WebTargetDS.java
+++ src/main/java/de/mediathekview/mlib/datasources/WebTargetDS.java
@@ -1,38 +1,36 @@
 /*
  * WebTargetDS.java
- * 
+ *
  * Projekt    : MServer
  * erstellt am: 05.10.2017
  * Autor      : Sascha
- * 
+ *
  * (c) 2017 by Sascha Wiegandt
  */
 package de.mediathekview.mlib.datasources;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 public class WebTargetDS {
-    
-    private static final Map<String, WebTarget> connectionPool = new ConcurrentHashMap<>();
-    
-    private WebTargetDS() {
-        
-    }
-    
-    public static WebTarget getInstance(String url) {
-        if (connectionPool.containsKey(url)) {
-            return connectionPool.get(url);
-        } else {
-            Client client = ClientBuilder.newClient();
-            connectionPool.put(url, client.target(url));
 
-            return connectionPool.get(url);
-        }
-    }
+  private static final Map<String, WebTarget> connectionPool = new ConcurrentHashMap<>();
 
+  private WebTargetDS() {}
+
+  public static WebTarget getInstance(final String url, final long timeoutInSeconds) {
+    if (connectionPool.containsKey(url)) {
+      return connectionPool.get(url);
+    } else {
+      final Client client =
+          ClientBuilder.newBuilder().readTimeout(timeoutInSeconds, TimeUnit.SECONDS).build();
+      connectionPool.put(url, client.target(url));
+
+      return connectionPool.get(url);
+    }
+  }
 }
