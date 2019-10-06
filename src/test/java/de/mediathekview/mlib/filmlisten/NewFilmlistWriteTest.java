3ce720ebdb0b1146fc680b12b2732231918a7af8
TheSasch <thesasch_github@wiegandt.net>
2019-10-06 22:21:38 +0200

    TestUmbau auf JUnit 5 und AssertJ.

--- src/test/java/de/mediathekview/mlib/filmlisten/NewFilmlistWriteTest.java
+++ src/test/java/de/mediathekview/mlib/filmlisten/NewFilmlistWriteTest.java
@@ -1,59 +1,58 @@
 package de.mediathekview.mlib.filmlisten;
 
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+import de.mediathekview.mlib.daten.Filmlist;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.io.TempDir;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
 public class NewFilmlistWriteTest {
-//  private static final String TEMP_FOLDER_NAME_PATTERN = "MLIB_TEST_%d";
-//  private static Filmlist testData;
-//  private static Path testFileFolderPath;
-//
-//  private static final FilmlistManager filmlistManager = FilmlistManager.getInstance();
-//  private final String jsonName;
-//  private final FilmlistFormats format;
-//
-//  public NewFilmlistWriteTest(final String aJsonName, final FilmlistFormats aFormat) {
-//    jsonName = aJsonName;
-//    format = aFormat;
-//  }
-//
-//  @Parameterized.Parameters(name = "Test {index} Filmlist for {0} with {1}")
-//  public static Collection<Object[]> data() {
-//    return Arrays.asList(new Object[][] {{"TestWriteNewJson_%d.json", FilmlistFormats.JSON},
-//        {"TestWriteNewJsonCompressed_%d.json.xz", FilmlistFormats.JSON_COMPRESSED_XZ},
-//        {"TestWriteNewJsonCompressed_%d.json.gz", FilmlistFormats.JSON_COMPRESSED_GZIP},
-//        {"TestWriteNewJsonCompressed_%d.json.bz", FilmlistFormats.JSON_COMPRESSED_BZIP},
-//        {"TestWriteOldJson_%d.json", FilmlistFormats.OLD_JSON},
-//        {"TestWriteOldJsonCompressed_%d.json.xz", FilmlistFormats.OLD_JSON_COMPRESSED_XZ},
-//        {"TestWriteOldJsonCompressed_%d.json.gz", FilmlistFormats.OLD_JSON_COMPRESSED_GZIP},
-//        {"TestWriteOldJsonCompressed_%d.json.bz", FilmlistFormats.OLD_JSON_COMPRESSED_BZIP}});
-//  }
-//
-//  @AfterClass
-//  public static void deleteTempFiles() throws IOException {
-//    Files.walk(testFileFolderPath).sorted(Comparator.reverseOrder()).map(Path::toFile)
-//        .forEach(File::delete);
-//  }
-//
-//  @BeforeClass
-//  public static void initTestData() throws URISyntaxException, IOException {
-//    testData = FilmlistTestData.getInstance().createTestdataNewFormat();
-//    testFileFolderPath = Files.createTempDirectory(formatWithDate(TEMP_FOLDER_NAME_PATTERN));
-//  }
-//
-//  private static String formatWithDate(final String aPattern) {
-//    return String.format(aPattern, new Date().getTime());
-//  }
-//
-//  @Test
-//  public void testWrite() {
-//    final Path testFilePath = testFileFolderPath.resolve(formatWithDate(jsonName));
-//    filmlistManager.save(format, testData, testFilePath);
-//
-//    if (format.getFileExtension().contains("xz")) {
-//      Assert.assertThat(
-//          Files.exists(testFilePath.resolveSibling(testFilePath.getFileName().toString())),
-//          CoreMatchers.is(true));
-//    } else {
-//      Assert.assertThat(Files.exists(testFilePath), CoreMatchers.is(true));
-//    }
-//  }
+  private static final String TEMP_FOLDER_NAME_PATTERN = "MLIB_TEST_%d";
+  private static Filmlist testData;
+
+  @TempDir
+  static Path testFileFolderPath;
+
+  private static final FilmlistManager filmlistManager = FilmlistManager.getInstance();
+
+  private static Stream<Arguments> createReadTestArguments() {
+    return Stream.of(
+        Arguments.of("TestFilmlistNewJson.json", FilmlistFormats.JSON),
+        Arguments.of("TestFilmlistNewJson.json.xz", FilmlistFormats.JSON_COMPRESSED_XZ),
+        Arguments.of("TestFilmlistNewJson.json.gz", FilmlistFormats.JSON_COMPRESSED_GZIP),
+        Arguments.of("TestFilmlistNewJson.json.bz", FilmlistFormats.JSON_COMPRESSED_BZIP),
+        Arguments.of("TestFilmlist.json", FilmlistFormats.OLD_JSON),
+        Arguments.of("TestFilmlist.json.xz", FilmlistFormats.OLD_JSON_COMPRESSED_XZ),
+        Arguments.of("TestFilmlist.json.gz", FilmlistFormats.OLD_JSON_COMPRESSED_GZIP),
+        Arguments.of("TestFilmlist.json.bz", FilmlistFormats.OLD_JSON_COMPRESSED_BZIP)
+    );
+  }
+
+  @BeforeAll
+  public static void initTestData() throws URISyntaxException, IOException {
+    testData = FilmlistTestData.getInstance().createTestdataNewFormat();
+  }
+
+  @ParameterizedTest
+  @MethodSource("createReadTestArguments")
+  public void testWrite(String jsonName, FilmlistFormats format) {
+    final Path testFilePath = testFileFolderPath.resolve(jsonName);
+    System.out.println(testFilePath.toString());
+    filmlistManager.save(format, testData, testFilePath);
+
+    if (format.getFileExtension().contains("xz")) {
+      assertThat(Files.exists(testFilePath.resolveSibling(testFilePath.getFileName().toString()))).isTrue();
+    } else {
+      assertThat(Files.exists(testFilePath)).isTrue();
+    }
+  }
 
 }
