8e75aa844396123a6ea690cce5ee2aac7d66c3ab
Nicklas Wiegandt <nicklas@wiegandt.eu>
2019-10-20 17:03:43 +0200

    Removed vulnerabilities.

--- src/main/java/de/mediathekview/mlib/tool/MVHttpClient.java
+++ src/main/java/de/mediathekview/mlib/tool/MVHttpClient.java
@@ -3,16 +3,12 @@ package de.mediathekview.mlib.tool;
 import okhttp3.ConnectionPool;
 import okhttp3.OkHttpClient;
 
-import javax.net.ssl.*;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
 import java.util.concurrent.TimeUnit;
 
 public class MVHttpClient {
   private static final MVHttpClient ourInstance = new MVHttpClient();
   private final OkHttpClient httpClient;
   private final OkHttpClient copyClient;
-  private final OkHttpClient sslUnsafeClient;
 
   private MVHttpClient() {
     httpClient =
@@ -31,8 +27,6 @@ public class MVHttpClient {
             .readTimeout(5, TimeUnit.SECONDS)
             .writeTimeout(2, TimeUnit.SECONDS)
             .build();
-
-    sslUnsafeClient = getUnsafeOkHttpClient();
   }
 
   public static MVHttpClient getInstance() {
@@ -45,60 +39,5 @@ public class MVHttpClient {
 
   public OkHttpClient getReducedTimeOutClient() {
     return copyClient;
-  }
-
-  public OkHttpClient getSSLUnsafeClient() {
-    return sslUnsafeClient;
-  }
-
-  /*
-   * Thanks to: https://gist.github.com/mefarazath/c9b588044d6bffd26aac3c520660bf40
-   */
-  private OkHttpClient getUnsafeOkHttpClient() {
-    try {
-      // Create a trust manager that does not validate certificate chains
-      final TrustManager[] trustAllCerts =
-          new TrustManager[] {
-            new X509TrustManager() {
-              @Override
-              public void checkClientTrusted(
-                  final java.security.cert.X509Certificate[] chain, final String authType)
-                  throws CertificateException {}
-
-              @Override
-              public void checkServerTrusted(
-                  final java.security.cert.X509Certificate[] chain, final String authType)
-                  throws CertificateException {}
-
-              @Override
-              public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                return new X509Certificate[0];
-              }
-            }
-          };
-
-      // Install the all-trusting trust manager
-      final SSLContext sslContext = SSLContext.getInstance("SSL");
-      sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
-      // Create an ssl socket factory with our all-trusting manager
-      final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
-
-      return new OkHttpClient.Builder()
-          .connectTimeout(5, TimeUnit.SECONDS)
-          .readTimeout(5, TimeUnit.SECONDS)
-          .writeTimeout(2, TimeUnit.SECONDS)
-          .sslSocketFactory(sslSocketFactory, (X509TrustManager) trustAllCerts[0])
-          .hostnameVerifier(
-              new HostnameVerifier() {
-                @Override
-                public boolean verify(final String hostname, final SSLSession session) {
-                  return true;
-                }
-              })
-          .build();
-
-    } catch (final Exception e) {
-      throw new RuntimeException(e);
-    }
   }
 }
