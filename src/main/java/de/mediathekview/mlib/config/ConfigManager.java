e3ee3691107e9920223687e64bcf6db0da379da8
Nicklas Wiegandt <nicklas@wiegandt.eu>
2021-03-13 17:24:51 +0100

    Changing ConfigManager to lazy read configuration when the first time getConfig is called

--- src/main/java/de/mediathekview/mlib/config/ConfigManager.java
+++ src/main/java/de/mediathekview/mlib/config/ConfigManager.java
@@ -1,33 +1,33 @@
 package de.mediathekview.mlib.config;
 
-
 import com.yacl4j.core.ConfigurationBuilder;
 
-/**
- * A manager to load configurations.
- */
-public abstract class ConfigManager<T extends ConfigDTO>
-{
-    private T config;
+/** A manager to load configurations. */
+public abstract class ConfigManager<T extends ConfigDTO> {
+  private T config;
 
-    protected abstract String getConfigFileName();
+  protected abstract String getConfigFileName();
 
-    protected abstract Class<T> getConfigClass();
+  protected abstract Class<T> getConfigClass();
 
-    protected ConfigManager()
-    {
-        
+  protected ConfigManager() {
+    config = null;
+  }
+
+  public void readConfig() {
+    config =
+        ConfigurationBuilder.newBuilder()
+            .source()
+            .fromFileOnClasspath(getConfigFileName())
+            .optionalSource()
+            .fromFileOnPath("./" + getConfigFileName())
+            .build(getConfigClass());
+  }
+
+  public T getConfig() {
+    if (config == null) {
+      readConfig();
     }
-
-    public void readConfig() {
-        config = ConfigurationBuilder.newBuilder()
-                .source().fromFileOnClasspath(getConfigFileName())
-                .optionalSource().fromFileOnPath("./"+getConfigFileName())
-                .build(getConfigClass());
-    }
-    
-    public T getConfig()
-    {
-        return config;
-    }
+    return config;
+  }
 }
